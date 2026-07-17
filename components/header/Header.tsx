997017bf7137e2e5846a96dfe2ba76d7b61eea25
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-07-17 21:32:26 +0530

    Added hero and header

---
+++ components/header/Header.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function Header() {
+  return (
+    <div className="absolute top-5 right-5">
+      <Button className="">
+        Portfolio
+      </Button>
+      <Button className="">
+        Get in Touch
+      </Button>
+    </div>
+  )
+}
---
+++ components/hero/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div className="text-6xl text-center">
+      <p>Where logic</p>
+      <p>becomes pixels</p>
+    </div>
+  )
+}
