3edf93a404ebbff818cd9c5f7188101448b4147a
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-07-17 21:35:57 +0530

    Moved the sections in their respective folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
-import Header from "@/components/header/Header";
-import Hero from "@/components/hero/Hero";
+import Header from "@/components/sections/header/Header";
+import Hero from "@/components/sections/hero/Hero";
 
 export default function Home() {
   return (
