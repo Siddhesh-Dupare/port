1ff267f793ad07851038c6536f87c81fd80ec2df
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-07-18 20:59:04 +0530

    Added about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 
 import Header from "@/components/sections/header/Header";
 import Hero from "@/components/sections/hero/Hero";
+import About from "@/components/sections/about/About";
 
 export default function Home() {
   return (
     <div className="relative h-screen">
       <Header />
-      <section className="flex h-full justify-center items-center">
+      <section className="min-h-screen flex justify-center items-center">
         <Hero />
       </section>
 
-      <section className="min-h-screen">
-        <p>Hello</p>
+      <section className="min-h-screen flex flex-col justify-center">
+        <About />
       </section>
+
     </div>
   );
 }
---
+++ components/sections/about/About.tsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div className="mx-5 sm:mx-10 lg:mx-20 max-auto max-w-3xl">
+      <div className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6 sm:mb-8 lg:mb-10">
+        About Me
+      </div>
+      <div className="text-base sm:text-lg lg:text-xl leading-relaxed">
+        <p className="space-y-4 sm:space-y-5 lg:space-y-6">
+          I&apos;m Siddhesh Dupare, a software developer with a strong interest in C++, desktop application development, and modern web technologies.
+        </p>
+
+        <p className="space-y-4 sm:space-y-5 lg:space-y-6">
+          I enjoy building software that solves real problems, especially developer tools, visualization systems, and applications that require performance and clean architecture.
+          I like understanding how software works internally—from algorithms and memory management to rendering and user interfaces.
+        </p>
+
+        <p className="space-y-4 sm:space-y-5 lg:space-y-6">Alongside C++, I work with React, Next.js, TypeScript, Node.js, and PostgreSQL to build full-stack applications.
+          I&apos;m always learning new technologies and enjoy turning ideas into polished, user-friendly software.
+        </p>
+
+        <p className="space-y-4 sm:space-y-5 lg:space-y-6">I&apos;m currently looking for opportunities where I can contribute to challenging engineering projects, grow as a developer, and collaborate with experienced teams.</p>
+      </div>
+    </div>
+  )
+}
