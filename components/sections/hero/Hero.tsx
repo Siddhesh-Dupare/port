9ae50a81c7a1a1328ed8e570085891646e528139
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-07-19 12:11:49 +0530

    Added gsap animation to heading page

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -1,14 +1,42 @@
+"use client";
 
 import { MoveRight, ArrowDown } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 
+import { useRef } from "react";
+
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(SplitText);
+
 export default function Hero() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    SplitText.create("h1", {
+      type: "lines",
+      mask: "lines",
+      linesClass: "split-child",
+      onSplit: (self) => {
+        return gsap.from(self.lines, {
+          duration: 1.5,
+          yPercent: 100,
+          ease: "power4",
+          stagger: 0.1,
+        });
+      }
+    });
+  }, { scope: containerRef});
+
   return (
-    <div className="relative h-full mx-2 text-primary">
-      <div className="text-6xl sm:text-9xl">
-        <p>Where logic</p>
-        <p>becomes pixels</p>
+    <div ref={containerRef} className="relative h-full mx-2 text-primary">
+      <div className="text-6xl sm:text-[11rem] sm:text-center uppercase">
+        <h1>Where logic</h1>
+        <h1 className="md:mx-50 text-left">becomes</h1>
+        <h1>pixels</h1>
       </div>
       <div className="absolute bottom-0 left-0 text-2xl">
         <div className="mb-10">
