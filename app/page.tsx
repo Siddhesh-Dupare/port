a7c1ad7057ce57b26ff6a4cd570fd75f00b82339
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-08-09 12:53:56 +0530

    Making the scroll effect

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <Hero />
       </section>
 
-      <section className="min-h-screen flex flex-col justify-center relative">
+      <section className="min-h-screen flex flex-col">
         <Projects />
       </section>
 
--- components/sections/projects/Projects.tsx
+++ components/sections/projects/Projects.tsx
@@ -2,23 +2,9 @@
 
 export default function Projects() {
   return (
-    <div className="relative mx-2 flex flex-col flex-1 my-24">
-      <div className="absolute top-0 left-2">
-        <h1 className="uppercase font-semibold text-4xl sm:text-8xl">projects</h1>
-      </div>
-      <div className="flex flex-col flex-1 px-2">
-        <div className="flex justify-between items-center text-2xl font-semibold">
-          <h1>AlgoLens</h1>
-          <p>2026</p>
-        </div>
-        <div style={{
-          flex: 1,
-          background: 'repeating-linear-gradient(45deg, #ff6a00 0px 20px, #ffdd00 20px 40px, #00c9ff 40px 60px, #00c9ff)'
-        }}>
-        </div>
-        <div className="text-center mt-4 text-2xl font-semibold">
-          A visualizer
-        </div>
+    <div className="relative mx-2">
+      <div className="uppercase font-semibold text-4xl sm:text-2xl">
+        projects
       </div>
     </div>
   );
