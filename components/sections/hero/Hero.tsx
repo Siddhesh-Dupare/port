f430a2cdb476db5bfdf97fa0d597106fc0f5f651
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-08-30 17:27:12 +0530

    Added text

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 export default function Hero() {
+
   return (
-    <div className="flex justify-center items-center h-screen p-6">
-      <svg viewBox="0 0 760 220" className="h-auto w-full select-none font-bold uppercase">
+    <div className="relative flex justify-center items-center h-screen p-6">
+      <svg viewBox="0 0 710 220" className="h-auto w-full select-none font-bold uppercase">
         <text x="0" y="0" dominantBaseline="hanging" fontSize="122" fill="currentColor">SID</text>
         <text x="190" y="8" fontSize="250" dominantBaseline="hanging" className="font-share-tech">D</text>
         <text x="310" y="0" fontSize="120" dominantBaseline="hanging" fill="currentColor">HESH</text>
